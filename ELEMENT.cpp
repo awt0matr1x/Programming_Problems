30c1bad16bf97209d17bc32f02281a70ce8b067f
AWTOMAT187 <142541947+AWTOMAT187@users.noreply.github.com>
2023-08-18 00:03:02 +0200

    Update ELEMENT.cpp

--- ELEMENT.cpp
+++ ELEMENT.cpp
@@ -1,1 +1,22 @@
 #include "ELEMENT.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "ELEMENT.h"
+
+
+#define ENDWERT 37
+#define STARTWERT 1
+#define REF 2
+
+
+int main() {
+	ELEMENT* hex_arr;
+	int ergebnis;
+	hex_arr = alloc_hex_arr(ENDWERT);
+	init_hex_arr(hex_arr,ENDWERT);
+
+	ergebnis = prod(ECKEN_ANZAHL, *(hex_arr+REF)->node);
+	printf("Produkt_Umkreis von Element %d = %d\n",REF, ergebnis);
+	
+	return 0;
+}
