240a98bcdd159fd700026f49b9af937f7eb1bfb5
AWTOMAT187 <142541947+AWTOMAT187@users.noreply.github.com>
2023-12-04 14:42:28 +0100

    Update ELEMENT.cpp
    COMMENTS

--- ELEMENT.cpp
+++ ELEMENT.cpp
@@ -1,21 +1,20 @@
 #include "ELEMENT.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "ELEMENT.h"
 
 
-#define ENDWERT 37
-#define STARTWERT 1
-#define REF 2
+#define ENDWERT 37		//ELEMENT ALLOCATION LIMITERS
+#define STARTWERT 1		
+#define REF 2			//ELEMENT'S OBJECT VALUE the calculation of surrounding ELEMENTS is meant for 
 
 
 int main() {
-	ELEMENT* hex_arr;
+	ELEMENT* hex_arr;					//DEFINITION OF AN ELEMENT_OBJECT (CONSTRUCTOR IN C++)
 	int ergebnis;
-	hex_arr = alloc_hex_arr(ENDWERT);
-	init_hex_arr(hex_arr,ENDWERT);
+	hex_arr = alloc_hex_arr(ENDWERT);			//ALLOCATION 
+	init_hex_arr(hex_arr,ENDWERT);				//INITIALATION
 
-	ergebnis = prod(ECKEN_ANZAHL, *(hex_arr+REF)->node);
+	ergebnis = prod(ECKEN_ANZAHL, *(hex_arr+REF)->node);	//OUTPUT
 	printf("Produkt_Umkreis von Element %d = %d\n",REF, ergebnis);
 	
 	return 0;
