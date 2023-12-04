dfab00171e40b9b31b78e011ad58e60061eeb518
AWTOMAT187 <142541947+AWTOMAT187@users.noreply.github.com>
2023-12-04 14:33:28 +0100

    Update ELEMENT.h
    COMMENTS

--- ELEMENT.h
+++ ELEMENT.h
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 
 
-typedef struct hexfeld {
+typedef struct hexfeld {						//DEFINITION OF AN ELEMENT OBJECT OF A HEX ARRAY
 	
 	unsigned int wert_ID,ring;
-	struct hexfeld* node[ECKEN_ANZAHL];
+	struct hexfeld* node[ECKEN_ANZAHL];				// N-POINTER AT SURROUNDING ELEMENTS, DEPENDING ON NUMBER OF CORNERS OF ELEMENT OBJECT   
 
 	void ring_ID(int element_ID) {
 		ring = (unsigned int)((wert_ID - 1) / ECKEN_ANZAHL);
@@ -17,12 +17,12 @@ typedef struct hexfeld {
 }ELEMENT;
 
 
-inline ELEMENT* alloc_hex_arr(unsigned int endwert) {
+inline ELEMENT* alloc_hex_arr(unsigned int endwert) {			//ALLOCATION OF ELEMENT OBJECTS
 
 	return (ELEMENT*)malloc(sizeof ELEMENT * endwert);
 }
 
-inline void init_hex_arr(ELEMENT* hex_arr, unsigned int endwert){
+inline void init_hex_arr(ELEMENT* hex_arr, unsigned int endwert){	//ITERATIV FILLING AND LINKING ALLOCATED ELEMENT OBJECTS WITH EACH CORRECT NEIGHBOUR DEPENDING ON GRAPHIC 1 
 	for (unsigned int i = 1; i <= endwert; i++) {
 		(hex_arr++)->wert_ID = i;
 		for (int j = 0; j < ECKEN_ANZAHL; j++) 
@@ -46,7 +46,7 @@ inline void init_hex_arr(ELEMENT* hex_arr, unsigned int endwert){
 		}
 	};
 
-inline unsigned int prod(unsigned int anzahl_nodes, ELEMENT* hex_element) {
+inline unsigned int prod(unsigned int anzahl_nodes, ELEMENT* hex_element) {	//ITERATIV CALCULATION OF PRODUCT OF SURROUNDING ELEMENT OBJECT VALUES 
 	int produkt = 1;
 	for (unsigned int i = 0; i < anzahl_nodes; i++)
 		produkt *= hex_element->node[i]->wert_ID;
