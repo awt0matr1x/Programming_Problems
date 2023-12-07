0b6404a70fa67b2984c5105a3607106b511c022b
AWTOMAT187 <142541947+AWTOMAT187@users.noreply.github.com>
2023-12-07 15:57:20 +0100

    Update ELEMENT.h

--- ELEMENT.h
+++ ELEMENT.h
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 #define ECKEN_ANZAHL 6    //SHOULD NOT BE CHANGED SINCE SPECIAL CASE (ECKEN_ANZAHL(number of corners) = NUMBER OF POSSIBLE ALIGNING NEIGHBOUR ELEMENT_OBJECT
 
 
@@ -31,11 +26,11 @@ inline void init_hex_arr(ELEMENT* hex_arr, unsigned int endwert){	//ITERATIV INI
 	hex_arr->cplx_exp = 0;
 	(hex_arr++)->abs = 0;						 //SPECIAL CASE FOR FIRST ELEMENT =i =1 (Point of Origin)
 	
-	for (unsigned int n = 1, j =0 ; i <= endwert; ++i,j++) {
+	for (unsigned int n = 1, j =0 ; i <= endwert; ++i,j++, hex_arr++) {		//
 		(hex_arr)->wert_ID = i;
 		
 		(hex_arr)->abs = n;		
-		(hex_arr++)->cplx_exp =(float)(360/(ECKEN_ANZAHL*n)*j;			//CALCULATING COMPLEX EXPONENTIAL VALUE MULTIPLIED BY ANGLE FACTOR j
+		(hex_arr)->cplx_exp =(float)(360/(ECKEN_ANZAHL*n))*j;			//CALCULATING COMPLEX EXPONENTIAL VALUE MULTIPLIED BY ANGLE FACTOR j
 		if(i==eor(n){								//TESTING THE CONDITION IF ITERATIV INITIALIZATION HAS REACHED END OF CURRENT RING 			
 			j=0;								//THUS SETTING ANGLE FACTOR BACK TO ZERO
 			n++;								//AND INCREMENTING THE ABS VALUE WHICH INDICATES THE NEXT RING
@@ -59,20 +54,18 @@ inline void link_hex_arr(ELEMENT* hex_arr, unsigned int endwert){	//ITERATIV FIL
 				switch(j){
 				 case 0:	 hex_arr->node[j] = hex_arr + (hex_arr->abs)*ECKEN_ANZAHL;  
 					break;
-				 case 1:	 hex_arr->node[j] = hex_arr + vektor(hex_arr-, abs, cplx_exp);
+				 case 1:	 hex_arr->node[j] = hex_arr + vektor(hex_arr,hex_arr->abs+1, hex_arr->cplx_exp)- hex_arr->wert_ID ;
 					break;
 				 case 2:	 hex_arr->node[j] = hex_arr + 1;
 					break;
 				 case 3:	 hex_arr->node[j] = hex_arr - 1; 
 					break;
-				 case 4:	 hex_arr->node[j] = hex_arr + (hex_arr->abs)*ECKEN_ANZAHL -1; 
+				 case 4:	 hex_arr->node[j] = hex_arr + (hex_arr->abs)*ECKEN_ANZAHL-1; 
 					break;
 				 case 5:	 hex_arr->node[j] = hex_arr + eor((hex_arr->abs)+1)-(hex_arr->wert_ID) ;
 					 break;
 				}
 			*/
-
-
 
 inline unsigned int prod(unsigned int anzahl_nodes, ELEMENT* hex_element) {	//ITERATIV CALCULATION OF PRODUCT OF SURROUNDING ELEMENT OBJECT VALUES 
 	int produkt = 1;
@@ -82,8 +75,7 @@ inline unsigned int prod(unsigned int anzahl_nodes, ELEMENT* hex_element) {	//IT
 }
 
 
-	
-unsigned int eor(unsigned int n){
+unsigned int eor(unsigned int n){				
 	unsigned int eor = 1;					//ITERATIV CALCULATION OF END OF RING 
 	for(int lv =1  ; lv<=n; lv++)				// EOR = SIG(upper limit = n|lower limit=n=1) n*ECKEN_ANZAHL
 	eor+=lv*ECKEN_ANZAHL;
@@ -92,7 +84,7 @@ unsigned int eor(unsigned int n){
 }
 
 
-unsigned int vektor(ELEMENT* hex_arr, int abs, int cplx_exp){
-	for(;hex_arr->abs != abs && hex_arr->cplx_exp != cplx_exp; hex_arr++)
+unsigned int vektor(ELEMENT* hex_arr, int abs, int cplx_exp){			//COMPUTATION OF COMPLEX OFFSET VEKTOR
+	for(;hex_arr->abs != abs && hex_arr->cplx_exp != cplx_exp; hex_arr++)	//ITERATE THROUGH COORDINATE SYSTEM ARRAY UNTIL COMPLEX VEKTOR IS FOUND, ACCORDING TO PARAMETERS 
 	return hex_arr->wert_ID;
 }
